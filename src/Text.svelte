620b00d1d2496c95aa7099f791c413dfbad019f2
Alexandre Pliarchopoulos <al.pliar@gmail.com>
2022-08-05 15:58:42 +0200

    style: fix text style

--- src/Text.svelte
+++ src/Text.svelte
@@ -5,7 +5,10 @@
 <style>
   .text {
     margin: 0;
+    font-size: var(--text-font-size);
     font-weight: var(--bold-font-weight);
     line-height: var(--big-line-height);
+    letter-spacing: -0.04em;
+    max-width: 648px;
   }
 </style>
